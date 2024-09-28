55368409cfa8c8abafaf77be86a905be5051e837
whdudgms <108641428+whdudgms@users.noreply.github.com>
2024-09-28 17:41:56 +0900

    [Silver IV] Title: 보물, Time: 176 ms, Memory: 17888 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Collections;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        int[] a = new int[n];
+        Integer[] b = new Integer[n];  // Integer 배열로 변경
+        
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            b[i] = sc.nextInt();
+        }
+        
+        Arrays.sort(a);
+        Arrays.sort(b, Collections.reverseOrder()); // Integer 배열이므로 reverseOrder 사용 가능
+        
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += (a[i] * b[i]);
+        }
+        System.out.println(sum);
+    }
+}
