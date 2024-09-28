b5d881455c02d76d6610e737fb2be66f79aeddd2
whdudgms <108641428+whdudgms@users.noreply.github.com>
2024-09-28 17:45:04 +0900

    [Silver IV] Title: 보물, Time: 100 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.java
+++ 백준/Silver/1026. 보물/보물.java
@@ -1,29 +1,23 @@
-import java.util.Scanner;
-import java.util.Arrays;
-import java.util.Collections;
-
-class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        
-        int[] a = new int[n];
-        Integer[] b = new Integer[n];  // Integer 배열로 변경
-        
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] A = new int[n];
+        int[] B = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer stk = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            a[i] = sc.nextInt();
+            A[i] = Integer.parseInt(st.nextToken());
+            B[i] = Integer.parseInt(stk.nextToken());
         }
-        for (int i = 0; i < n; i++) {
-            b[i] = sc.nextInt();
-        }
-        
-        Arrays.sort(a);
-        Arrays.sort(b); // Integer 배열이므로 reverseOrder 사용 가능
-        
+        Arrays.sort(A);
+        Arrays.sort(B);
         int sum = 0;
         for (int i = 0; i < n; i++) {
-            sum += (a[i] * b[n-i-1]);
+            sum += A[i] * B[n-1-i];
         }
-        System.out.println(sum);
+        System.out.print(sum);
     }
 }
