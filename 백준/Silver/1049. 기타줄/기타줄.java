ac46f95018340449b4edf53bea88c627aa50bf97
whdudgms <108641428+whdudgms@users.noreply.github.com>
2025-01-25 13:17:36 +0900

    [Silver IV] Title: 기타줄, Time: 120 ms, Memory: 15740 KB -BaekjoonHub

--- 백준/Silver/1049. 기타줄/기타줄.java
+++ 백준/Silver/1049. 기타줄/기타줄.java
@@ -6,47 +6,40 @@ import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 class Main{
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
         StringTokenizer num = new StringTokenizer(br.readLine());
-        
-         // 끊어진 줄 
         int a = Integer.parseInt(num.nextToken());
-        // 기타줄 브랜드의 갯수     
-        int b =  Integer.parseInt(num.nextToken());
+        int b = Integer.parseInt(num.nextToken());
         
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
         
-        int min1 = -1;
-        int min2 = -1;
-        
-        StringTokenizer st;
         for(int i = 0; i < b; i++){
-            st = new StringTokenizer(br.readLine());
-            int num1 =  Integer.parseInt(st.nextToken());
-            int num2 =  Integer.parseInt(st.nextToken());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int num1 = Integer.parseInt(st.nextToken());
+            int num2 = Integer.parseInt(st.nextToken());
             
-            min1 = min1 == -1 || min1 > num1 ?  num1 : min1;
-            min2 = min2 == -1 || min2 > num2 ?  num2 : min2;
+            min1 = Math.min(min1, num1);
+            min2 = Math.min(min2, num2);
         }
         
-        //9개 12개 이런식으로 끊어질 수 있는개  6개 마다로 계산해서 출력하나보다. 
-        
-        // 낱개 
         int result = 0;
         
-        if(a % 6 > 0){
-            result += min1 > (min2 * (a % 6)) ? (min2*(a % 6)) : min1;
-            a -= a % 6;
-        }
+        int case1 = min2 * a;
+        
+        int case2 = ((a + 5) / 6) * min1;
+        
+        int case3 = (a/6)* min1 + (a % 6) * min2;
         
         
-        // 몫
-        if(a / 6 > 0)
-           result += min1 * (a / 6 ) > (min2 * a) ? (min2 * (a)) : min1*( a / 6 );
-            
-        bw.write(result+ "\n");
+        result = Math.min(case1,  Math.min(case2, case3));
+    
+        bw.write(result+"\n");
+        bw.flush();
         bw.close();
- 
+        
     }
-} 
+}
