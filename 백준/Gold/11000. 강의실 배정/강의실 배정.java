142085ee2364d44581dbfc40cac8e80108e15ca3
whdudgms <108641428+whdudgms@users.noreply.github.com>
2025-03-28 15:49:29 +0900

    [Gold V] Title: 강의실 배정, Time: 600 ms, Memory: 71592 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        // (1) 입력받은 강의의 우선순위를 정해준다.
+        PriorityQueue<int[]> pq = new PriorityQueue<>(((o1, o2) -> {
+            if(o1[0] == o2[0]){
+                return o1[1] - o2[1];
+            }else{
+                return o1[0] - o2[0];
+            }
+        }));
+        // (2) 강의를 입력받아서 pq에 저장한다.
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            pq.offer(new int[]{start, end});
+        }
+        // (3) 강의실을 나타내고, 강의의 종료시간을 담을 우선순위 큐 room을 만든다.
+        PriorityQueue<Integer> room = new PriorityQueue<>();
+        room.offer(0);
+        
+        // (4) room에서 하나씩 값을 peek() 하여 값을 비교하고, poll()을 할지 말지 결정한다.
+        while(!pq.isEmpty()){
+            int[] cur = pq.poll();
+            if(room.peek() <= cur[0]){
+                room.poll();
+            }
+            room.offer(cur[1]);
+        }
+
+        System.out.println(room.size());
+    }
+}
