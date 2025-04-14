a4ee1df34b5c8102fd56ddc1b5b88a14c67576ad
whdudgms <108641428+whdudgms@users.noreply.github.com>
2025-04-14 11:32:28 +0900

    [Silver II] Title: 유기농 배추, Time: 148 ms, Memory: 16132 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
+    static int M,N,K,count;
+    static int[][] map;
+    static boolean[][] visited;
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        
+        while(T-->0){
+            st = new StringTokenizer(br.readLine());
+            M = Integer.parseInt(st.nextToken());
+            N = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
+            map = new int[N][M];
+            visited = new boolean[N][M];
+            count = 0;
+            
+            for(int i = 0; i < K; i++){
+                st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                map[b][a] = 1;
+            }
+            
+            for(int i = 0; i < N; i++){
+                for(int j = 0; j < M; j++){
+                    if(!visited[i][j] && map[i][j] == 1){
+                        dfs(i,j);
+                        count++;
+                    }
+                }
+            }
+            bw.append(count+"\n");
+        }
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+    
+    static void dfs(int x, int y){
+        visited[x][y] = true;
+        for(int i = 0; i < 4; i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if(nx < 0 || ny < 0 || nx>= N || ny >= M) continue;
+            
+            if(!visited[nx][ny] && map[nx][ny] ==1){
+                dfs(nx,ny);
+            }
+        }
+    }
+}
