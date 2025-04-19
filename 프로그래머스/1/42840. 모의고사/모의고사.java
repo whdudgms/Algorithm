8cc04f926bc6f4829e8bf3c9667cc285511d9672
whdudgms <108641428+whdudgms@users.noreply.github.com>
2025-04-19 23:01:22 +0900

    [level 1] Title: 모의고사, Time: 3.82 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,40 +1,31 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 
 class Solution {
     public int[] solution(int[] answers) {
         
-        int[] first = {1,2,3,4,5}; // 5개씩 반복 
-        int[] second = {2,1,2,3,2,4,2,5}; // 8개씩 반복 
-        int[] third = {3,3,1,1,2,2,4,4,5,5}; // 10개씩 반복 
-        int[] score = {0,0,0}; // 각 수포자들의 점수
-
+        int[] a = {1, 2, 3, 4, 5};
+        int[] b = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] c = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         
-        // 1, 2, 3, 4, 5                    :1번
-        //2, 1, 2, 3, 2, 4, 2, 5            :2번
-        // 3, 3, 1, 1, 2, 2, 4, 4, 5, 5     :3번 
         
-        for(int i=0; i<answers.length; i++) {
-            if(answers[i] == first[i%5]) score[0]++;
-            if(answers[i] == second[i%8]) score[1]++;
-            if(answers[i] == third[i%10]) score[2]++;
+        int an = 0;
+        int bn = 0;
+        int cn = 0;
+        
+        for (int i = 0; i < answers.length; i++) {
+            if (answers[i] == a[i % a.length]) an++;
+            if (answers[i] == b[i % b.length]) bn++;
+            if (answers[i] == c[i % c.length]) cn++;
         }
+        int max = Math.max(an, Math.max(bn, cn));
+        ArrayList<Integer> list = new ArrayList<>();
         
-        //최대 점수 구하기 
-        int max = Math.max(score[0], Math.max(score[1], score[2]));
+        if (an == max) list.add(1);
+        if (bn == max) list.add(2);
+        if (cn == max) list.add(3);
         
-        //최대 점수를 가진 수포자 리스트 생성 
-        List<Integer> answ = new ArrayList<Integer>();
-        for(int i=0; i<score.length; i++) if(max == score[i]) answ.add(i+1);
-        
-        int[] answer = new int[answ.size()];
-        for(int i=0; i<answ.size(); i++){
-            answer[i] = answ.get(i);
-        }
-
-        
-        
-        return answer;
+        // int[]로 변환
+        return list.stream().mapToInt(i -> i).toArray();
     }
 }
