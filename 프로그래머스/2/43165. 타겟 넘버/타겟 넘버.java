7effb667fa3fe646681011b33c74465a93c3482d
whdudgms <108641428+whdudgms@users.noreply.github.com>
2025-04-20 13:37:36 +0900

    [level 2] Title: 타겟 넘버, Time: 5.53 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,28 +1,19 @@
-
-
 class Solution {
-    int count = 0;
+    int answer = 0;
     public int solution(int[] numbers, int target) {
-        int answer = 0;
-        dfs(numbers, 0, target, 0);
-        answer = count;
+        dfs(0, numbers, target, 0);
         
         return answer;
     }
-    public void dfs(int[] numbers, int depth, int target, int result){
-        if (depth == numbers.length){ //마지막 노드까지 진행했을 때
-            if (target == result){ //target값과 합계가 같다면
-                count++;
+    
+    private void dfs(int depth, int[] numbers, int target, int sum){
+        if(depth == numbers.length){
+            if(sum == target){
+                answer++;
             }
             return;
         }
-        
-        int plus = result + numbers[depth]; //양수를 더한 값
-        int minus = result - numbers[depth]; //음수를 더한 값
-        
-        dfs(numbers, depth+1, target, plus);
-        dfs(numbers, depth+1, target, minus);
-        
+        dfs(depth + 1, numbers, target, sum + numbers[depth]);  
+        dfs(depth + 1, numbers, target, sum - numbers[depth]);
     }
-    
 }
