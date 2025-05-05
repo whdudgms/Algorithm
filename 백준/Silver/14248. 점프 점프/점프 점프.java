db580ccf8508f3a5d1e1592b28071b89649cf088
whdudgms <108641428+whdudgms@users.noreply.github.com>
2025-05-05 21:07:16 +0900

    [Silver II] Title: 점프 점프, Time: 236 ms, Memory: 25168 KB -BaekjoonHub

--- 백준/Silver/14248. 점프 점프/점프 점프.java
+++ 백준/Silver/14248. 점프 점프/점프 점프.java
@@ -1,45 +1,57 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-public class Main{
+public class Main {
     static int N, S;
     static int[] arr;
     static boolean[] visited;
     static int result;
-    
-    public static void main(String[] args) throws Exception{
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
+
         N = Integer.parseInt(br.readLine());
         arr = new int[N + 1];
         visited = new boolean[N + 1];
-        result = 1;
-        
+
         st = new StringTokenizer(br.readLine());
-        for(int i = 1; i <= N; i++) {
+        for (int i = 1; i <= N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
+
         S = Integer.parseInt(br.readLine());
-        
-        visited[S] = true;
-        dfs(S);
+
+        result = bfs(S);
 
         System.out.println(result);
     }
-    static void dfs(int start) {
-        int front = start - arr[start];
-        int back = start + arr[start];
-        if(back <= N && !visited[back]) {
-            visited[back] = true;
-            result++;
-            dfs(back);
+
+    static int bfs(int start) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+        visited[start] = true;
+        int count = 1;
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+
+            int left = current - arr[current];
+            int right = current + arr[current];
+
+            if (left > 0 && !visited[left]) {
+                visited[left] = true;
+                queue.add(left);
+                count++;
+            }
+
+            if (right <= N && !visited[right]) {
+                visited[right] = true;
+                queue.add(right);
+                count++;
+            }
         }
-        
-        if(front > 0 && !visited[front]) {
-            visited[front] = true;
-            result++;
-            dfs(front);
-        }
+
+        return count;
     }
 }
