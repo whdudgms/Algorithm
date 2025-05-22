bf5f1a713eca021fc54028bd42a9b106660a53f2
whdudgms <108641428+whdudgms@users.noreply.github.com>
2025-05-22 18:42:24 +0900

    [Silver I] Title: 숨바꼭질, Time: 152 ms, Memory: 18972 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,29 +1,34 @@
 import java.util.*;
 import java.io.*;
 
+
 public class Main{
-    
     static int N;
     static int K;
     static int[] check = new int[100001];
     
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        K = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         
-        if(N == K){
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        if (N == K) {
             System.out.println(0);
         } else {
             bfs(N);
         }
+        
     }
     
     
-    static void bfs(int num){
+    public static void bfs(int num){
+        
         Queue<Integer> q = new LinkedList<>();
         q.add(num);
         check[num] = 1;
+        
         
         while(!q.isEmpty()){
             int temp = q.poll();
@@ -31,29 +36,26 @@ public class Main{
             for(int i = 0; i < 3; i++){
                 int next;
                 
+                
                 if(i == 0){
                     next = temp +1;
-                } else if(i == 1){
+                }else if(i == 1){
                     next = temp -1;
-                } else {
-                    next = temp *2;
+                } else{
+                    next = temp * 2;
                 }
-                
                 
                 if(next == K){
                     System.out.println(check[temp]);
                     return;
                 }
                 
-                
-                if(next >= 0 && next < check.length && check[next] == 0){
+                if(next >= 0  && next < check.length && check[next] == 0){
                     q.add(next);
                     check[next] = check[temp] +1;
                 }
             }
-        }
+        }           
     }
-    
-    
     
 }
