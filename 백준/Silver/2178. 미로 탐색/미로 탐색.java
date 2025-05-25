c5dc3cce4a53ce47424cabd1d3460b8f92d3941c
whdudgms <108641428+whdudgms@users.noreply.github.com>
2025-05-25 14:09:51 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14632 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -3,37 +3,36 @@ import java.io.*;
 
 public class Main{
     
-    static int[][] map;
-    static int n;
-    static int m;
+    static int[][] graph;
+    static int n, m;
     static boolean[][] visited;
-    
-    static int[] dx = {-1,1,0,0};
+    static int[] dx = {1,-1,0,0};
     static int[] dy = {0,0,-1,1};
     
     public static void main(String[] args) throws IOException{
         
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
         StringTokenizer st = new StringTokenizer(br.readLine());
         
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         
-        map = new int[n][m];
+        graph = new int[n][m];
         
         for(int i = 0; i < n; i++){
-            String s = br.readLine();
+            String s =  br.readLine();
             for(int j = 0; j < m; j++){
-                map[i][j] = s.charAt(j) - '0';
+                graph[i][j] = s.charAt(j) - '0';
             }
         }
-        
         visited = new boolean[n][m];
+
         visited[0][0] = true;
         bfs(0,0);
-        
-        System.out.println(map[n-1][m-1]);
+        System.out.println(graph[n - 1][m - 1]);
     }
+    
     
     public static void bfs(int x, int y){
         Queue<int[]> q = new LinkedList<>();
@@ -48,16 +47,16 @@ public class Main{
                 int nextX = nowX + dx[i];
                 int nextY = nowY + dy[i];
                 
-                if(nextX < 0 || nextY < 0 || nextX >= n || nextY >= m) continue;
                 
-                if(visited[nextX][nextY] || map[nextX][nextY] == 0) continue;
+                     if(nextX < 0 || nextY < 0 || nextX >= n || nextY >= m) continue;
                 
+                if(visited[nextX][nextY] || graph[nextX][nextY]  == 0) continue;
                 
                 q.add(new int[]{nextX, nextY});
-                map[nextX][nextY] = map[nowX][nowY] +1;
-                visited[nextX][nextY] = true;
-                
+                graph[nextX][nextY] = graph[nowX][nowY] +1;
+                visited[nextX][nextY]= true;
             }
+            
         }
     }
     
