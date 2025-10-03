1a43c986191aae1027a54b641adc3dd7dcf3a977
whdudgms <108641428+whdudgms@users.noreply.github.com>
2025-10-03 22:47:36 +0900

    [level 1] Title: 평균 구하기, Time: 0.05 ms, Memory: 70.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        
+        for(int i = 0; i < arr.length; i++){
+            answer += arr[i];
+        }
+        
+        answer = answer / (double) arr.length;
+        
+        return answer;
+    }
+}
