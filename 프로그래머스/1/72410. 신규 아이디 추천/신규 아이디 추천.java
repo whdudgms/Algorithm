a1eb61ee3e3d3d6a6751c2bd67d760eff00d3141
whdudgms <108641428+whdudgms@users.noreply.github.com>
2025-10-26 17:19:16 +0900

    [level 1] Title: 신규 아이디 추천, Time: 16.63 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String solution(String new_id) {
+        String answer = new_id.toLowerCase();
+        
+        answer = answer.replaceAll("[^-_.a-z0-9]", "");
+        answer = answer.replaceAll("[.]{2,}", ".");
+        answer = answer.replaceAll("^[.]|[.]$","");
+        
+        if(answer.equals("")){
+            answer += "a";
+        }
+        if (answer.length() >= 16) {     // 6단계
+            answer = answer.substring(0, 15);
+            answer = answer.replaceAll("[.]$","");
+        }
+        if (answer.length() <= 2) {  // 7단계
+            while (answer.length() < 3) {
+                answer += answer.charAt(answer.length()-1);
+            }
+        }
+        return answer;
+    }
+}
