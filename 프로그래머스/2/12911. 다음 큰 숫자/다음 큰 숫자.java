4201d429a089abb15f89c1777414ce924d1a3dd1
whdudgms <108641428+whdudgms@users.noreply.github.com>
2026-01-26 14:44:16 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.04 ms, Memory: 91.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int n) {
+        
+        int count  =  Integer.bitCount(n);
+        
+        int answer = n + 1;
+        
+        while(true){
+            if( count == Integer.bitCount(answer)){
+                break;
+            }
+            answer++;
+        }
+        
+        return answer;
+    }
+}
