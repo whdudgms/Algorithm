00ac67f9e4483ad4a89c2fead192998f92c6c217
whdudgms <108641428+whdudgms@users.noreply.github.com>
2026-02-02 10:03:55 +0900

    [level 2] Title: 귤 고르기, Time: 69.05 ms, Memory: 94.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        
+        int sum = 0;
+        int cnt = 0;
+        
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int num : tangerine) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        
+        ArrayList<Integer> valueList = new ArrayList<>(map.values());
+        Collections.sort(valueList, Collections.reverseOrder());
+        for(int v : valueList){
+            if(sum + v >= k){
+                cnt++;
+                break;
+            }else{
+                sum += v;
+                cnt++;
+            }
+        }
+    
+        return cnt;
+    }
+}
